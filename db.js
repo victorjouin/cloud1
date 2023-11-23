acf2f4c15fc5f42a44e5e24c87e5595ecf9e72a1
victorjouin <davidmoresta1@gmail.com>
2023-11-23 10:45:39 +0100

    cpnnard

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-  `${process.env.INURL}`, // database connection string
+  `${process.env.INURL}`, // TODO: database connection string
   {
     dialect: 'postgres',
     dialectOptions: {
@@ -18,13 +18,7 @@ const sequelize = new Sequelize(
   },
 )
 
-sequelize.authenticate().then(() => {
-  console.log('Connection has been established successfully.');
-})
-.catch(err => {
-  console.error('Unable to connect to the database:', err);
-});
-
+sequelize.authenticate()
 sequelize.sync()
 
 module.exports = sequelize
